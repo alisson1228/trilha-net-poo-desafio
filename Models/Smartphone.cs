89273f9fe32b92f09dedd745126588bc6bfce4f2
Alisson <alisson12souza@hotmail.com>
2024-01-05 22:27:25 -0300

    Adicionar herança e implementar testes

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,21 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
         }
+
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
+        public string Numero { get; set; } 
+        public string Modelo => _modelo;
+        public string IMEI => _imei;
+        public int Memoria => _memoria;
 
         public void Ligar()
         {
@@ -19,6 +26,11 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+
+        public void SobreDispositivo()
+        {
+            Console.WriteLine($"Numero: {Numero}\nModelo: {_modelo}\nIMEI: {_imei}\nMemoria: {_memoria}");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia sp1 = new Nokia(numero: "(88) 99999-9999", modelo: "S10", imei: "4444444", memoria: 128);
+Iphone sp2 = new Iphone(numero: "(85) 99999-9999", modelo: "I15MAX", imei: "5555555", memoria: 64);
+
+Console.WriteLine("Smartphone Nokia\n");
+
+sp1.Ligar();
+sp1.SobreDispositivo();
+sp1.InstalarAplicativo("Whatsapp");
+
+
+Console.WriteLine("\nSmartphone Iphone\n");
+
+sp2.ReceberLigacao();
+Console.WriteLine("Numero: " + sp2.Numero);
+Console.WriteLine("Modelo: " + sp2.Modelo);
+Console.WriteLine("IMEI: " + sp2.IMEI);
+Console.WriteLine("Memoria: " + sp2.Memoria);
+
+sp2.InstalarAplicativo("Youtude");
+
+sp1.Numero = "(88) 99666-6666";
+
+Console.WriteLine("\nSmartphone Nokia\n");
+
+sp1.SobreDispositivo();
